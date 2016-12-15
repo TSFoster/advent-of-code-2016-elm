74129b91cec2803184dc186b34f2f564f2c61410
T.S. Foster <hi@tsf.io>
2016-12-15 16:03:49 +0000

    Answer day 5 part 2

--- DayFive.elm
+++ DayFive.elm
@@ -8,6 +8,7 @@ import Debug exposing (log)
 answers : List QandA
 answers =
     [ QandA (Question "Day 5 part 1") part1
+    , QandA (Question "Day 5 part 2") part2
     ]
 
 
@@ -19,9 +20,22 @@ part1 =
         )
 
 
+part2 : Answer
+part2 =
+    Uncalculated
+        (\() ->
+            getSecondPassword "uqwqemis"
+        )
+
+
 getPassword : String -> String
 getPassword doorId =
     findNextChars 8 doorId 0 |> String.fromList
+
+
+getSecondPassword : String -> String
+getSecondPassword doorId =
+    findCharsForIndexes (List.repeat 8 ' ') doorId 4515059 |> String.fromList
 
 
 findNextChars : Int -> String -> Int -> List Char
@@ -30,14 +44,35 @@ findNextChars count prefix i =
         []
     else
         let
-            ( next, j ) =
+            ( next, _, j ) =
                 findNextChar prefix i
         in
             (log ("Found character with index " ++ toString j ++ "! " ++ toString (count - 1) ++ " more to go") next)
                 :: findNextChars (count - 1) prefix (j + 1)
 
 
-findNextChar : String -> Int -> ( Char, Int )
+findCharsForIndexes : List Char -> String -> Int -> List Char
+findCharsForIndexes list prefix i =
+    if not (List.member ' ' list) then
+        list
+    else
+        let
+            ( index, next, j ) =
+                findNextChar prefix i
+        in
+            case String.fromChar index |> String.toInt |> Result.toMaybe of
+                Nothing ->
+                    findCharsForIndexes list prefix (j + 1)
+
+                Just pos ->
+                    let
+                        newList =
+                            log ("Status as of " ++ toString j) (setCharIfEmpty pos next list)
+                    in
+                        findCharsForIndexes newList prefix (j + 1)
+
+
+findNextChar : String -> Int -> ( Char, Char, Int )
 findNextChar prefix i =
     let
         logProgress =
@@ -50,14 +85,39 @@ findNextChar prefix i =
             hex (prefix ++ toString i)
     in
         if hash |> String.startsWith "00000" then
-            ( sixthPos hash, i )
+            ( getPos 6 hash, getPos 7 hash, i )
         else
             findNextChar prefix (i + 1)
 
 
-sixthPos : String -> Char
-sixthPos =
+getPos : Int -> String -> Char
+getPos i =
     String.toList
-        >> List.drop 5
+        >> List.drop (i - 1)
         >> List.head
         >> Maybe.withDefault ' '
+
+
+setCharIfEmpty : Int -> Char -> List Char -> List Char
+setCharIfEmpty pos char list =
+    let
+        before =
+            List.take pos list
+
+        atPos =
+            List.drop pos list |> List.head
+
+        after =
+            List.drop (pos + 1) list
+    in
+        case atPos of
+            Nothing ->
+                list
+
+            Just c ->
+                case c of
+                    ' ' ->
+                        before ++ [ char ] ++ after
+
+                    _ ->
+                        list
