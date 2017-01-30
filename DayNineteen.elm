010a42bcdd2e6cd00168f277f997ebe723f0ab05
T.S. Foster <hi@tsf.io>
2017-01-30 10:03:42 +0000

    Answer day 19 part 2

--- DayNineteen.elm
+++ DayNineteen.elm
@@ -6,6 +6,7 @@ import Model exposing (..)
 answers : List QandA
 answers =
     [ QandA (Question "Day 19 part 1") part1
+    , QandA (Question "Day 19 part 2") part2
     ]
 
 
@@ -17,9 +18,26 @@ part1 =
         )
 
 
+part2 : Answer
+part2 =
+    Uncalculated
+        (\() ->
+            toString <| elfWinsSecond puzzleInput
+        )
+
+
 elfWins : Int -> Int
 elfWins i =
     2 * (i - 2 ^ (floor (logBase 2 (toFloat i)))) + 1
+
+
+elfWinsSecond : Int -> Int
+elfWinsSecond n =
+    let
+        lastPowerOfThree =
+            3 ^ (floor (logBase 3 (toFloat n - 0.5)))
+    in
+        min lastPowerOfThree (n - lastPowerOfThree) + max 0 (2 * (n - 2 * lastPowerOfThree))
 
 
 puzzleInput : Int
